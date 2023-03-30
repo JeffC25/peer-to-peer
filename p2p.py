859afdaa8e585cf4774192c1ce6d94b104aa7a07
Jeffrey Chen <34695547+JeffC25@users.noreply.github.com>
2023-03-30 04:34:46 -0400

    comments

--- p2p.py
+++ p2p.py
@@ -8,13 +8,16 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'localhost'
 sock.bind((host, 0))
 
+# display address info
 print(f"Host: {sock.getsockname()[0]}")
 print(f"Socket: {sock.getsockname()[1]}")
 
+# ask user to wait or input address to connect to
 action = input("connect or wait: ")
 while not (action == "connect" or action == "wait"):
     action = input("connect or wait: ")
 
+# connect to a host
 if action == "connect":
     print("Connecting")
     connectHost = input("Host: ")
@@ -22,16 +25,20 @@ if action == "connect":
     sock.connect((connectHost, connectPort))
     print(f"Connected to {connectHost}:{connectPort}")
 
+    # send message
     message = input("Enter message: ")
     sock.sendall(message.encode())
 
+# wait for connection
 else:
     sock.listen(1)
     print('Waiting for a connection...')
     connection, clientAddress = sock.accept()
     print(f"Connected to {clientAddress}")
 
+    # recieve message
     data = connection.recv(1024)
     print(f"Message from {clientAddress}:", data.decode())
 
+# close socket
 sock.close()
