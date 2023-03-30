8b848e19d11ea761e7323a821f69f3ab1b316e99
JeffC25 <jefflichen25@gmail.com>
2023-03-30 10:41:51 -0400

    implement classes

--- p2p.py
+++ p2p.py
@@ -1,37 +1,71 @@
-import asyncio
 import socket
 
-# create socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Peer():
+    def __init__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = sock
 
-# bind the socket to a port and address
-host = 'localhost'
-sock.bind((host, 0))
+        host = "localhost"
+        sock.bind((host, 0))
 
-print(f"Host: {sock.getsockname()[0]}")
-print(f"Socket: {sock.getsockname()[1]}")
 
-action = input("connect or wait: ")
-while not (action == "connect" or action == "wait"):
-    action = input("connect or wait: ")
+    def displayAddress(self):
+        print(f"Host: {self.sock.getsockname()[0]}")
+        print(f"Ports: {self.sock.getsockname()[1]}")
+    
 
-if action == "connect":
-    print("Connecting")
-    connectHost = input("Host: ")
-    connectPort = int(input("Port: "))
-    sock.connect((connectHost, connectPort))
-    print(f"Connected to {connectHost}:{connectPort}")
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+        print(f"Connected to {host}:{port}")
+        return host, port
 
-    message = input("Enter message: ")
-    sock.sendall(message.encode())
 
-else:
-    sock.listen(1)
-    print('Waiting for a connection...')
-    connection, clientAddress = sock.accept()
-    print(f"Connected to {clientAddress}")
+    def wait(self):
+        self.sock.listen(1)
+        print('Waiting for a connection...')
+        connection, clientAddress = self.sock.accept()
+        print(f"Connected to {clientAddress}")
+        return connection, clientAddress
+    
 
-    data = connection.recv(1024)
-    print(f"Message from {clientAddress}:", data.decode())
+    def sendMessage(self, message):
+        self.sock.sendall(message.encode())
 
-sock.close()
+
+    def recieveMessage(self, connection, address):
+        data = connection.recv(1024)
+        print(f"Message from {address}:", data.decode())
+
+    def close(self):
+        self.sock.close()
+
+def connOrWait():
+    action = input("Connect (c) or wait (w): ")
+    while not (action == "c" or action == "w"):
+        action = input("Connect (c) or wait (w): ")
+    return action
+
+
+def main():
+    peer = Peer()
+
+    peer.displayAddress()
+
+    action = connOrWait()
+
+    if action == 'c':
+        host = input("Host: ")
+        port = int(input("Port: "))
+        peer.connect(host, port)
+        
+        message = input("Enter message: ")
+        peer.sendMessage(message)
+
+    else:
+        connection, address = peer.wait()
+        peer.recieveMessage(connection, address)
+
+
+
+if __name__ == '__main__':
+    main()
